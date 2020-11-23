03dbcb634993a86bce092508fdf2e981472363b8
fengqiang <330538422@qq.com>
2020-11-23 15:22:55 +0800

    [mod] 卡片样式修改

--- src/components/TestCard/index.vue
+++ src/components/TestCard/index.vue
@@ -97,7 +97,7 @@
       <tr v-for="row in testData.mtr_list.rows" :key="row">
         <td>{{ row }}</td>
         <td v-for="(item, index) in testData.mtr_list[row]" :key="index">
-          {{ index !== testData.mtr_list[row].length - 1 ? item.value : '' }} <br>
+          {{ index !== testData.mtr_list[row].length - 1 ? item.value : '' }}
           {{
             index !== testData.mtr_list[row].length - 1 ? item.add_subtract : item.status
           }}
