f0763676cffdd6a123042edcac6c22972c475381
e <813212927@qq.com>
2020-12-21 10:50:07 +0800

    row.bound === '+'

--- src/views/equipment-management/spare-part/warehousing.vue
+++ src/views/equipment-management/spare-part/warehousing.vue
@@ -301,6 +301,8 @@ export default {
     tableRowClassName({ row, rowIndex }) {
       if (row.bound === '-') {
         return 'warning-row'
+      } else if (row.bound === '+') {
+        return 'max-warning-row'
       }
       return ''
     }
