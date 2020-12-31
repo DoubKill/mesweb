48da06e49d9d1085038821c6186fa50e38fd444e
fengqiang <330538422@qq.com>
2020-12-31 09:53:17 +0800

    [add] 卡片添加备注信息

--- src/components/TestCard/index.vue
+++ src/components/TestCard/index.vue
@@ -45,7 +45,7 @@
           <td>收皮员</td>
           <td>{{ testData.operation_user }}</td>
           <td>备注</td>
-          <td />
+          <td>{{ testData.test ? testData.test.pallet_no : '' }}</td>
         </tr>
         <tr>
           <td colspan="4">质量信息卡</td>
