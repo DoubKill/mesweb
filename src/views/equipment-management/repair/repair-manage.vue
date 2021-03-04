fd5d00df0b6766b700e3c3606f5b308c30bc89d5
e <813212927@qq.com>
2021-03-04 11:32:16 +0800

    图片只能上传一张加提示

--- src/views/equipment-management/repair/repair-manage.vue
+++ src/views/equipment-management/repair/repair-manage.vue
@@ -269,6 +269,7 @@
             list-type="picture-card"
             :on-preview="handlePictureCardPreview"
             :on-change="onChangeImg"
+            :on-exceed="onExceed"
             :limit="1"
           >
             <i class="el-icon-plus" />
@@ -461,6 +462,9 @@ export default {
         }).catch(e => {
           this.$message.error('指派失败')
         })
+    },
+    onExceed() {
+      this.$message.info('最多上传一张图片')
     },
     onChangeImg(file, fileList) {
       const isJPG = ['image/jpeg', 'image/jpg', 'image/png'].includes(file.raw.type)
