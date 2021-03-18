6b525df162de3ebdabd86b782d5431fe80c2bb79
e <813212927@qq.com>
2021-03-18 10:33:36 +0800

    批量人工出库按钮加权限

--- src/views/inventory/compound-rubber-manage.vue
+++ src/views/inventory/compound-rubber-manage.vue
@@ -50,7 +50,11 @@
     </el-form>
     <el-button v-permission="['compoundRubber_plan','norman']" class="button-right" @click="normalOutbound">正常出库</el-button>
     <el-button v-permission="['compoundRubber_plan','assign']" class="button-right" @click="assignOutbound">指定出库</el-button>
-    <el-button class="button-right" @click="manualDeliveryBatch">批量人工出库</el-button>
+    <el-button
+      v-permission="['compoundRubber_plan','manual']"
+      class="button-right"
+      @click="manualDeliveryBatch"
+    >批量人工出库</el-button>
     <el-button class="button-right" @click="getList">刷新</el-button>
 
     <el-table
