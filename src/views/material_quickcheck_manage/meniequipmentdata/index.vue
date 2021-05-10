43abf4ae8e4fe0df3576bd3601f61debf2fec54d
mayuanyuan <2994981966@qq.com>
2021-05-10 17:13:12 +0800

    ll

---
+++ src/views/material_quickcheck_manage/meniequipmentdata/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    门尼设备数据绑定管理
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  created() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang='less'>
+
+</style>
