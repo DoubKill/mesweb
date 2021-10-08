7207fa5e2964ee0bd8b0e6d39daa946d19991d45
e <813212927@qq.com>
2021-10-08 15:39:23 +0800

    提交

--- src/views/equipment-manage/master-data/parts-define.vue
+++ src/views/equipment-manage/master-data/parts-define.vue
@@ -69,26 +69,152 @@
     >
       <el-table-column
         prop="date"
-        label="信号编号"
+        label="所属主设备种类"
         min-width="20"
       />
+      <el-table-column
+        prop="date"
+        label="所属设备部位"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="部件分类"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="部件代码"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="部件名称"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="是否已绑定备件(Y/N)"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="是否启用"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="录入者"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="录入时间"
+        min-width="20"
+      />
+      <el-table-column label="操作" width="250px">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="showEditDialog(scope.row)"
+          >绑定备件</el-button>
+          <el-button
+            size="mini"
+            @click="showEditDialog(scope.row)"
+          >编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            plain
+            @click="handleDelete(scope.row)"
+          >{{ scope.row.used_flag?'停用':'启用' }}
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
+    <page
+      :old-page="false"
+      :total="total"
+      :current-page="formInline.page"
+      @currentChange="currentChange"
+    />
+    <el-dialog
+      :title="`${dialogForm.id?'编辑':'新建'}设备部件定义`"
+      :visible.sync="dialogVisible"
+      width="900px"
+      :before-close="handleClose"
+    >
+      <el-form
+        ref="createForm"
+        :rules="rules"
+        label-width="150px"
+        :model="dialogForm"
+      >
+        <el-form-item
+          label="信号编号"
+          prop="name"
+        >
+          <el-select v-model="dialogForm.value" placeholder="请选择">
+            <el-option
+              v-for="item in []"
+              :key="item"
+              :label="item"
+              :value="item"
+            />
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import page from '@/components/page'
 export default {
   name: 'EquipmentMasterDataPartsDefine',
+  components: { page },
   data() {
     return {
       formInline: {},
       loading: false,
-      tableData: []
+      tableData: [{}],
+      dialogVisible: false,
+      total: 0,
+      dialogForm: {},
+      rules: {
+        name: [{ required: true, message: '不能为空', trigger: 'blur' }],
+        type: [{ required: true, message: '不能为空', trigger: 'change' }]
+      }
     }
   },
   methods: {
+    async getList() {
+      try {
+        this.loading = true
+        // const data = await testIndicators('get', null, { all: 1 })
+        // this.tableData = data || []
+        this.loading = false
+      } catch (e) {
+        this.loading = false
+      }
+    },
+    currentChange(page, pageSize) {
+      this.formInline.page = page
+      this.formInline.page_size = pageSize
+      this.getList()
+    },
     onSubmit() {
 
+    },
+    showEditDialog() {
+      this.dialogVisible = true
+    },
+    handleClose(done) {
+      this.dialogVisible = false
+      this.$refs.createForm.resetFields()
+      if (done) {
+        done()
+      }
     }
   }
 }
