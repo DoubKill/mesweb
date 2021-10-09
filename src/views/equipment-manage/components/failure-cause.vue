899733d18e99fc88b0bb956601666235dd0df6a0
e <813212927@qq.com>
2021-10-09 15:35:20 +0800

    提交

---
+++ src/views/equipment-manage/components/failure-cause.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <!-- 停机故障原因选择 列表 -->
+    <el-form inline>
+      <el-form-item label="故障大类名称">
+        <el-input v-model="dataForm.name" />
+      </el-form-item>
+      <el-form-item label="故障代码">
+        <el-input v-model="dataForm.name" />
+      </el-form-item>
+      <el-form-item label="故障名称">
+        <el-input v-model="dataForm.name" />
+      </el-form-item>
+    </el-form>
+    <el-table
+      ref="multipleTable"
+      v-loading="loading"
+      :data="tableData"
+      style="width: 100%"
+      row-key="id"
+      border
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column
+        type="selection"
+        width="55"
+        :reserve-selection="true"
+        :selectable="()=>{return true}"
+      />
+      <el-table-column
+        prop="date"
+        label="故障大类代码"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="故障大类名称"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="分类代码"
+        min-width="20"
+      />
+      <el-table-column
+        prop="date"
+        label="分类名称"
+        min-width="20"
+      />
+    </el-table>
+    <page
+      :old-page="false"
+      :total="total"
+      :current-page="dataForm.page"
+      @currentChange="currentChange"
+    />
+  </div>
+</template>
+
+<script>
+import page from '@/components/page'
+export default {
+  components: { page },
+  data() {
+    return {
+      dataForm: {},
+      tableData: [],
+      total: 0,
+      loading: false
+    }
+  },
+  methods: {
+    async getList() {
+      try {
+        this.loading = true
+        // const data = await equipmentProperty('get', null, { params: this.search })
+        // this.tableData = data.results || []
+        // this.total = data.count
+        this.loading = false
+      } catch (e) {
+        this.loading = false
+      }
+    },
+    currentChange(page, page_size) {
+      this.dataForm.page = page
+      this.dataForm.page_size = page_size
+      this.getList()
+    },
+    handleSelectionChange() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/equipment-manage/master-data/fault-classify.vue
+++ src/views/equipment-manage/master-data/fault-classify.vue
@@ -1,163 +1,221 @@
 <template>
   <div class="fault-classify">
     <!-- 设备故障分类管理 -->
-    <el-form :inline="true">
-      <el-form-item label="故障大类名称">
-        <el-input v-model="formInline.user" clearable placeholder="故障大类名称" />
-      </el-form-item>
-      <el-form-item label="故障代码">
-        <el-input v-model="formInline.user" clearable placeholder="故障代码" />
-      </el-form-item>
-      <el-form-item label="故障名称">
-        <el-input v-model="formInline.user" clearable placeholder="故障名称" />
-      </el-form-item>
-      <el-form-item label="是否启用">
-        <el-select v-model="formInline.region" clearable placeholder="是否启用">
-          <el-option label="区域一" value="shanghai" />
-          <el-option label="区域二" value="beijing" />
-        </el-select>
-      </el-form-item>
-      <el-form-item style="float:right">
-        <el-button type="primary" @click="onSubmit">导出Excel</el-button>
-        <el-button type="primary" @click="onSubmit">导入Excel</el-button>
-        <el-button type="primary" @click="onSubmit">新建</el-button>
-      </el-form-item>
-    </el-form>
-    <el-table
-      v-loading="loading"
-      :data="tableData"
-      style="width: 100%"
-      border
-    >
-      <el-table-column
-        prop="date"
-        label="故障大类代码"
-        min-width="20"
-      />
-      <el-table-column
-        prop="name"
-        label="故障大类名称"
-        min-width="20"
-      />
-      <el-table-column
-        prop="address"
-        label="分类代码"
-        min-width="20"
-      />
-      <el-table-column
-        prop="date"
-        label="分类名称"
-        min-width="20"
-      />
-      <el-table-column
-        prop="name"
-        label="是否启用"
-        min-width="20"
-      />
-      <el-table-column
-        prop="address"
-        label="录入者"
-        min-width="20"
-      />
-      <el-table-column
-        prop="address"
-        label="录入时间"
-        min-width="20"
-      />
-      <el-table-column label="操作" width="200px">
-        <template slot-scope="scope">
-          <el-button-group>
+    <el-row class="search-form-style">
+      <el-col v-loading="loading" :span="10">
+        <el-form inline>
+          <el-form-item
+            label="大分类故障名称"
+          >
+            <el-input v-model="dataForm.name" />
+          </el-form-item>
+          <el-form-item
+            label="是否启用"
+          >
+            <el-select v-model="dataForm.value" style="width:130px">
+              <el-option
+                v-for="item in []"
+                :key="item"
+                :label="item"
+                :value="item"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item>
             <el-button
-              size="mini"
-              @click="showEditDialog(scope.row)"
-            >编辑</el-button>
+              type="primary"
+              style="margin-bottom:10px"
+              @click="addArea(true)"
+            >添加</el-button>
+          </el-form-item>
+        </el-form>
+
+        <el-table
+          ref="currentRow"
+          :data="tableData"
+          border
+          highlight-current-row
+          @current-change="handleCurrentChange"
+        >
+          <el-table-column
+            type="index"
+            label="序号"
+          />
+          <el-table-column
+            prop="depot_name"
+            label="大分类故障代码"
+          />
+          <el-table-column
+            prop="description"
+            label="大分类故障名称"
+          />
+          <el-table-column
+            prop="description"
+            label="是否使用"
+          />
+          <el-table-column
+            label="操作"
+          >
+            <template slot-scope="scope">
+              <el-button-group>
+                <el-button
+                  size="mini"
+                  @click="editArea(scope.row,true)"
+                >编辑
+                </el-button>
+                <el-button
+                  size="mini"
+                  type="danger"
+                  plain
+                  @click.stop="delArea(scope.row,true)"
+                >{{ scope.row.use_flag ? '停用' : '启用' }}
+                </el-button>
+              </el-button-group>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+      <el-col v-loading="loading1" :span="14">
+        <el-form inline style="float:right">
+          <el-form-item
+            label="中分类故障名称"
+          >
+            <el-input v-model="dataForm1.name" />
+          </el-form-item>
+          <el-form-item
+            label="是否启用"
+          >
+            <el-select v-model="dataForm1.value">
+              <el-option
+                v-for="item in []"
+                :key="item"
+                :label="item"
+                :value="item"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item>
             <el-button
-              size="mini"
-              type="danger"
-              plain
-              @click="handleDelete(scope.row)"
-            >{{ scope.row.used_flag?'停用':'启用' }}
-            </el-button>
-          </el-button-group>
-        </template>
-      </el-table-column>
-    </el-table>
-    <page
-      :old-page="false"
-      :total="total"
-      :current-page="formInline.page"
-      @currentChange="currentChange"
-    />
+              style="margin-bottom:10px;float:right"
+              :disabled="depot?false:true"
+              type="primary"
+              @click="addArea(false)"
+            >添加</el-button>
+          </el-form-item>
+        </el-form>
+        <el-table
+          :data="tableData1"
+          border
+        >
+          <el-table-column
+            type="index"
+            label="序号"
+          />
+          <el-table-column
+            prop="depot__depot_name"
+            label="中分类故障代码"
+          />
+          <el-table-column
+            prop="depot_site_name"
+            label="中分类故障名称"
+          />
+          <el-table-column
+            prop="description"
+            label="备注"
+          />
+          <el-table-column
+            prop="depot_site_name"
+            label="是否使用"
+          />
+          <el-table-column
+            label="操作"
+          >
+            <template slot-scope="scope">
+              <el-button-group>
+                <el-button
+                  size="mini"
+                  @click="editArea(scope.row,false)"
+                >编辑
+                </el-button>
+                <el-button
+                  size="mini"
+                  type="danger"
+                  plain
+                  @click.stop="delArea(scope.row,false)"
+                >{{ scope.row.use_flag ? '停用' : '启用' }}
+                </el-button>
+              </el-button-group>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
     <el-dialog
-      :title="`${dialogForm.id?'编辑':'新建'}设备部位定义`"
+      :title="(formObj.id?'编辑':'添加')+(isType?'大分类故障':'中分类故障')"
       :visible.sync="dialogVisible"
-      width="500px"
+      width="30%"
       :before-close="handleClose"
     >
       <el-form
-        ref="createForm"
+        ref="formObj"
+        :model="formObj"
         :rules="rules"
-        label-width="120px"
-        :model="dialogForm"
+        label-width="150px"
       >
-        <el-form-item
-          label="故障大类代码"
-          prop="type"
-        >
-          <el-select v-model="dialogForm.type" placeholder="请选择">
-            <el-option
-              v-for="item in ['浙江','大连']"
-              :key="item"
-              :label="item"
-              :value="item"
-            />
-          </el-select>
+        <el-form-item v-if="!isType" label="中分类故障代码" prop="depot_name">
+          <el-input v-model="formObj.depot_name" />
         </el-form-item>
-        <el-form-item
-          label="故障大类名称"
-          prop="name"
-        >
-          <el-input v-model="dialogForm.name" :disabled="dialogForm.id?true:false" />
+        <el-form-item v-if="!isType" label="中分类故障名称" prop="depot_name">
+          <el-input v-model="formObj.depot_name" />
         </el-form-item>
-        <el-form-item
-          label="故障分类代码"
-          prop="name"
-        >
-          <el-input v-model="dialogForm.name" :disabled="dialogForm.id?true:false" />
+        <el-form-item v-if="!isType" label="备注" prop="depot_site_name">
+          <el-input v-model="formObj.depot_site_name" />
         </el-form-item>
-        <el-form-item
-          label="故障分类名称"
-          prop="name"
-        >
-          <el-input v-model="dialogForm.name" />
+        <el-form-item v-if="isType" label="大分类故障代码" prop="description">
+          <el-input v-model="formObj.description" />
+        </el-form-item>
+        <el-form-item v-if="isType" label="大分类故障名称" prop="description">
+          <el-input v-model="formObj.description" />
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="handleClose(false)">取 消</el-button>
-        <el-button type="primary" :loading="btnLoading" @click="submitFun">确 定</el-button>
+        <el-button type="primary" :loading="loadingBtn" @click="submitFun">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import page from '@/components/page'
 export default {
   name: 'EquipmentMasterDataFaultClassify',
-  components: { page },
   data() {
     return {
-      formInline: {},
       tableData: [{}],
-      total: 0,
-      loading: false,
+      tableData1: [{}],
+      dataForm: {},
+      dataForm1: {},
+      formObj: {},
       dialogVisible: false,
+      loading: false,
+      loading1: false,
+      depot: null,
+      // true 为大分类故障
+      isType: false,
       rules: {
-        name: [{ required: true, message: '不能为空', trigger: 'blur' }],
-        type: [{ required: true, message: '不能为空', trigger: 'change' }]
-      },
-      dialogForm: {},
-      btnLoading: false
+        depot_name: [
+          { required: true, message: '请输入库区', trigger: 'blur' }
+        ],
+        depot_site_name: [
+          { required: true, message: '请输入库位', trigger: 'blur' }
+        ],
+        description: [
+          { required: true, message: '请输入描述', trigger: 'blur' }
+        ],
+        depot: [
+          { required: true, message: '请选择库区', trigger: 'change' }
+        ]
+      }
     }
   },
   methods: {
@@ -171,58 +229,56 @@ export default {
         this.loading = false
       }
     },
-    currentChange(page, pageSize) {
-      this.formInline.page = page
-      this.formInline.page_size = pageSize
-      this.getList()
+    handleCurrentChange(row) {
+      this.depot = row.id
     },
-    onSubmit() {
+    editArea(row, bool) {
       this.dialogVisible = true
+      this.isType = bool
     },
-    showEditDialog() {
-      this.dialogVisible = true
+    handleClose(done) {
+      this.$refs.formObj.resetFields()
+      done()
     },
-    handleDelete: function(row) {
-      var str = row.used_flag ? '停用' : '启用'
+    submitFun() {
+      this.$refs.formObj.validate(async(valid) => {
+        if (valid) {
+          try {
+            this.loadingBtn = true
+            // const _api = this.isArea ? sulfurDepot : sulfurDepotSite
+            // const _method = this.formObj.id ? 'put' : 'post'
+            // await _api(_method, this.formObj.id, { data: this.formObj })
+            // this.$message.success('操作成功')
+            // this.handleClose(false)
+            // if (this.isArea) {
+            //   this.getList()
+            // } else {
+            //   this.getList1()
+            // }
+            this.loadingBtn = false
+          } catch (e) {
+            this.loadingBtn = false
+          }
+        } else {
+          return false
+        }
+      })
+    },
+    delArea(row, bool) {
+      var str = row.delete_flag ? '启用' : '停用'
       this.$confirm('此操作将' + str + row.name + ', 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        // deleteLocation(row.id)
-        //   .then(response => {
-        //     this.$message({
-        //       type: 'success',
-        //       message: '操作成功!'
-        //     })
-        //     this.getList()
+        try {
+        //   departmentManage('delete', row.id, {
+        //   }).then(response => {
+        //     this.$message({ message: '操作成功', type: 'success' })
+        //     this.currentChange()
         //   })
-      })
-    },
-    handleClose(done) {
-      this.dialogVisible = false
-      this.$refs.createForm.resetFields()
-      if (done) {
-        done()
-      }
-    },
-    submitFun() {
-      this.$refs.createForm.validate(async(valid) => {
-        if (valid) {
-          try {
-            this.btnLoading = true
-            // const _api = this.bindingForm.id ? 'put' : 'post'
-            // await equipPart(_api, this.bindingForm.id || null, { data: this.bindingForm })
-            this.$message.success('操作成功')
-            this.handleClose(null)
-            this.getList()
-            this.btnLoading = false
-          } catch (e) {
-            this.btnLoading = false
-          }
-        } else {
-          return false
-        }
+        } catch (e) { e }
+      }).catch(() => {
       })
     }
   }
