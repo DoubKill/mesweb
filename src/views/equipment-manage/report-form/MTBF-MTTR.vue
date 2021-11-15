45388605b7fa40145cfb00a6b8e29360ac7aa641
吴恒 <813212927@qq.com>
2021-11-15 09:56:44 +0800

    新增 部分报修路由

---
+++ src/views/equipment-manage/report-form/MTBF-MTTR.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- MTBF/MTTR分析报表 -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/equipment-manage/report-form/detailed.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 订单明细报表 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EquipmentReportFormDetailed'
+}
+</script>
+
+<style>
+
+</style>
