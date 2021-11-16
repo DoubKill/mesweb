cc0bd24375ea1f02bebb1db627b51a6f71b95095
吴恒 <813212927@qq.com>
2021-11-16 09:06:59 +0800

    提交 部分报表列表

--- src/views/equipment-manage/report-form/order-completed.vue
+++ src/views/equipment-manage/report-form/order-completed.vue
@@ -1,12 +1,120 @@
 <template>
   <div>
-    <!-- 订单按时完成报表 -->
+    <!-- 订单按时完成率报表 -->
+    <el-form :inline="true">
+      <el-form-item label="起止日期">
+        <el-date-picker
+          v-model="dateValue"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          value-format="yyyy-MM-dd"
+          @change="changeDate"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          :loading="btnExportLoad"
+          @click="exportTable"
+        >导出Excel</el-button>
+        <el-button type="primary" @click="getList">查询</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      v-loading="loading"
+      :data="tableData"
+      border
+    >
+      <el-table-column
+        prop="order_id"
+        label="订单类别"
+        min-width="20"
+      />
+      <el-table-column
+        prop="order_id"
+        label="巡检"
+        min-width="20"
+      />
+      <el-table-column
+        prop="order_id"
+        label="保养"
+        min-width="20"
+      />
+      <el-table-column
+        prop="order_id"
+        label="润滑"
+        min-width="20"
+      />
+      <el-table-column
+        prop="order_id"
+        label="标定"
+        min-width="20"
+      />
+      <el-table-column
+        prop="order_id"
+        label="维修"
+        min-width="20"
+      />
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'EquipmentReportFormOrderCompleted'
+  name: 'EquipmentReportFormOrderCompleted',
+  data() {
+    return {
+      dateValue: [],
+      btnExportLoad: false,
+      search: {},
+      loading: false,
+      tableData: []
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    async getList() {
+      try {
+        this.loading = true
+        const obj = {}
+        Object.assign(obj, this.search)
+        // const data = await equipWarehouseRecord('get', null, { params: obj })
+        // this.tableData = data.results || []
+        // this.total = data.count
+        this.loading = false
+      } catch (e) {
+        this.loading = false
+      }
+    },
+    changeDate(arr) {
+      this.search.s_time = arr ? arr[0] : ''
+      this.search.e_time = arr ? arr[1] : ''
+      this.getList()
+    },
+    exportTable() {
+      this.btnExportLoad = true
+      // const obj = Object.assign({ export: 1 }, this.search)
+      // const _api = equipWarehouseRecord
+      // _api('get', null, { params: obj, responseType: 'blob' })
+      //   .then(res => {
+      //     const link = document.createElement('a')
+      //     const blob = new Blob([res], { type: 'application/vnd.ms-excel' })
+      //     link.style.display = 'none'
+      //     link.href = URL.createObjectURL(blob)
+      //     link.download = '备件出入库履历.xlsx' // 下载的文件名
+      //     document.body.appendChild(link)
+      //     link.click()
+      //     document.body.removeChild(link)
+      //     this.btnExportLoad = false
+      //   }).catch(e => {
+      //     this.btnExportLoad = false
+      //   })
+    }
+  }
 }
 </script>
 
