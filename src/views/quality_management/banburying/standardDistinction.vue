afbe3fe5aaa58ddac7c391a71de501a50c4a9a37
吴恒 <813212927@qq.com>
2021-11-22 10:52:02 +0800

    提交 投料防错路由

---
+++ src/views/quality_management/banburying/standardDistinction.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 公差标准区分关键字 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BanburyingStandardDistinction'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/quality_management/banburying/toleranceEntry.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 配方公差标准录入 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BanburyingCurrency'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
