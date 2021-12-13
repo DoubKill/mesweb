1c4f3968bf844ece48514881248414f38207569d
吴恒 <813212927@qq.com>
2021-12-13 13:43:52 +0800

    添加 自动排程路由

---
+++ src/views/plan/schedule/dispose/machine-production-all.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 机台生产计划-整体列表 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScheduleMachineProduction'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/plan/schedule/dispose/machine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 机台生产计划-机台列表 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScheduleMachine'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
