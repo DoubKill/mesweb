5e6acd733b8bc01e237960ced7a801184fdaa6ac
qiny.jiang <270052939@qq.com>
2022-03-08 09:13:27 +0800

    排程物流库存单

--- src/views/plan/schedule/dispose/scheduled-inventory.vue
+++ src/views/plan/schedule/dispose/scheduled-inventory.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="scheduledInventory">
+  <div>
     <!-- 排程无硫库存单 -->
     <el-form :inline="true">
       <el-form-item label="工厂日期">
@@ -17,168 +17,53 @@
       </el-form-item>
       <el-form-item>
         <el-button v-permission="['aps_stock_summary','export']" type="primary" @click="exportTable">导出Excel</el-button>
-        <el-button v-permission="['aps_stock_summary','export']" type="primary" @click="exportTable">保存</el-button>
-        <el-button v-permission="['aps_stock_summary','export']" type="primary" @click="exportTable">新建</el-button>
       </el-form-item>
     </el-form>
-    <el-table
-      id="out-table"
-      v-loading="loading"
-      :data="tableData"
-      border
-    >
-      <el-table-column
-        align="center"
-        prop="product_no"
-        label="规格"
-        min-width="20"
-      />
-      <el-table-column
-        align="center"
-        prop="HMB"
-        label="HMB（kg）"
-        min-width="40"
+    <div style="width:70%">
+      <el-table
+        id="out-table"
+        v-loading="loading"
+        :data="tableData"
+        border
       >
         <el-table-column
           align="center"
-          prop="GK400_pt"
-          label="库内库存"
+          prop="product_no"
+          label="规格"
           min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_pt }}</span>
-            <el-input-number v-else v-model="row.GK400_pt" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
+        />
         <el-table-column
           align="center"
-          prop="GK400_dj"
-          label="现场库存"
+          prop="HMB"
+          label="HMB（kg）"
           min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_dj }}</span>
-            <el-input-number v-else v-model="row.GK400_dj" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
-      </el-table-column>
-      <el-table-column
-        align="center"
-        prop="CMB"
-        label="CMB（kg）"
-        min-width="40"
-      >
+        />
         <el-table-column
           align="center"
-          prop="GK400_pt"
-          label="库内库存"
+          prop="CMB"
+          label="CMB（kg）"
           min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_pt }}</span>
-            <el-input-number v-else v-model="row.GK400_pt" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
+        />
         <el-table-column
           align="center"
-          prop="GK400_dj"
-          label="现场库存"
+          prop="1MB"
+          label="1MB（kg）"
           min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_dj }}</span>
-            <el-input-number v-else v-model="row.GK400_dj" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
-      </el-table-column>
-      <el-table-column
-        align="center"
-        prop="1MB"
-        label="1MB（kg）"
-        min-width="40"
-      >
+        />
         <el-table-column
           align="center"
-          prop="GK400_pt"
-          label="库内库存"
+          prop="2MB"
+          label="2MB（kg）"
           min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_pt }}</span>
-            <el-input-number v-else v-model="row.GK400_pt" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
+        />
         <el-table-column
           align="center"
-          prop="GK400_dj"
-          label="现场库存"
+          prop="3MB"
+          label="3MB（kg）"
           min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_dj }}</span>
-            <el-input-number v-else v-model="row.GK400_dj" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
-      </el-table-column>
-      <el-table-column
-        align="center"
-        prop="2MB"
-        label="2MB（kg）"
-        min-width="40"
-      >
-        <el-table-column
-          align="center"
-          prop="GK400_pt"
-          label="库内库存"
-          min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_pt }}</span>
-            <el-input-number v-else v-model="row.GK400_pt" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
-        <el-table-column
-          align="center"
-          prop="GK400_dj"
-          label="现场库存"
-          min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_dj }}</span>
-            <el-input-number v-else v-model="row.GK400_dj" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
-      </el-table-column>
-      <el-table-column
-        align="center"
-        prop="3MB"
-        label="3MB（kg）"
-        min-width="40"
-      >
-        <el-table-column
-          align="center"
-          prop="GK400_pt"
-          label="库内库存"
-          min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_pt }}</span>
-            <el-input-number v-else v-model="row.GK400_pt" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
-        <el-table-column
-          align="center"
-          prop="GK400_dj"
-          label="现场库存"
-          min-width="20"
-        >
-          <template slot-scope="{row}">
-            <span v-if="loading">{{ row.GK400_dj }}</span>
-            <el-input-number v-else v-model="row.GK400_dj" controls-position="right" :min="0" :max="99.99" :precision="2" />
-          </template>
-        </el-table-column>
-      </el-table-column>
-    </el-table>
+        />
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -211,21 +96,13 @@ export default {
     changeList() {
       this.$debounce(this, 'getList')
     },
-    async exportTable() {
-      await this.$set(this, 'loading', true)
-      await exportExcel('排程无硫库存单')
-      setTimeout(() => {
-        this.loading = false
-      }, 1000)
+    exportTable() {
+      exportExcel('排程无硫库存单')
     }
   }
 }
 </script>
 
-<style lang="scss">
-.scheduledInventory{
-        .el-input-number{
-            width:120px;
-        }
-    }
+<style lang="scss" scoped>
+
 </style>
