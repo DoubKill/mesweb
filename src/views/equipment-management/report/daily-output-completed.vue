19c345d0b52996b5bde8542ff30751dfaa123c50
qiny.jiang <270052939@qq.com>
2022-04-28 15:49:23 +0800

    文字修改

--- src/views/equipment-management/report/daily-output-completed.vue
+++ src/views/equipment-management/report/daily-output-completed.vue
@@ -587,7 +587,7 @@ export default {
           const blob = new Blob([res], { type: 'application/vnd.ms-excel' })
           link.style.display = 'none'
           link.href = URL.createObjectURL(blob)
-          link.download = '月常量完成.xlsx' // 下载的文件名
+          link.download = '月产量完成.xlsx' // 下载的文件名
           document.body.appendChild(link)
           link.click()
           document.body.removeChild(link)
