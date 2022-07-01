0319e8ccc8878d869d4d3fd2a54db76c1a60aaec
吴恒 <813212927@qq.com>
2022-07-01 14:53:19 +0800

    综合合格率年度汇总 加折线图

--- src/views/quality_management/pass/year-ummary.vue
+++ src/views/quality_management/pass/year-ummary.vue
@@ -48,12 +48,19 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <div
+      id="passRateLine"
+      class="volumeBox"
+      style="width: 100%;height:400px;margin-top:20px"
+    />
   </div>
 </template>
 
 <script>
 import { productSynthesisMonthRate } from '@/api/base_w_five'
 import { setDate, exportExcel } from '@/utils'
+import * as echarts from 'echarts'
 export default {
   name: 'YearUmmary',
   data() {
@@ -62,6 +69,80 @@ export default {
       tableData: [],
       search: {
         s_time: setDate(false, false, 'month')
+      },
+      optionPassRateLine: {
+        title: {
+          left: 'left',
+          text: '综合合格率年度汇总',
+          textStyle: { color: '#000' }
+        },
+        legend: {
+          data: ['综合合格率', '一次合格率', '流变合格率'],
+          textStyle: {
+            color: '#000'// 字体颜色
+          }
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            // type: 'cross'
+          }
+        },
+        grid: {
+          top: '25%',
+          left: '3%',
+          right: '6%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            axisTick: {
+              alignWithLabel: true
+            },
+            data: [],
+            axisLine: {
+              lineStyle: {
+                color: '#000',
+                width: 1
+              }
+            }
+          }
+        ],
+        yAxis: {
+          type: 'value',
+          name: '%',
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: '#000',
+              width: 1
+            }
+          },
+          max: 100,
+          min: 0
+        },
+        series: [
+          {
+            name: '综合合格率',
+            type: 'line',
+            data: [],
+            itemStyle: { normal: { label: { show: true }}}
+          },
+          {
+            name: '一次合格率',
+            type: 'line',
+            data: [],
+            itemStyle: { normal: { label: { show: true }}}
+          },
+          {
+            name: '流变合格率',
+            type: 'line',
+            data: [],
+            itemStyle: { normal: { label: { show: true }}}
+          }
+        ]
       }
     }
   },
@@ -76,6 +157,34 @@ export default {
         const arr = await productSynthesisMonthRate('get', null, { params: { year: aa }})
         this.loading = false
         this.tableData = arr.data || []
+
+        const _month = []
+        const qualified_rate_data_rate = []
+        const qualified_rate_data_qualified_count = []
+        const qualified_rate_data_total = []
+        for (let index = 1; index < 13; index++) {
+          _month.push(index + '月')
+        }
+        this.tableData.forEach((d, i) => {
+          for (let index = 1; index < 13; index++) {
+            if (i === 0) {
+              qualified_rate_data_rate.push(d[index])
+            }
+            if (i === 1) {
+              qualified_rate_data_qualified_count.push(d[index])
+            }
+            if (i === 2) {
+              qualified_rate_data_total.push(d[index])
+            }
+          }
+        })
+        this.optionPassRateLine.series[0].data = qualified_rate_data_rate
+        this.optionPassRateLine.series[1].data = qualified_rate_data_qualified_count
+        this.optionPassRateLine.series[2].data = qualified_rate_data_total
+        this.optionPassRateLine.xAxis[0].data = _month
+
+        this.myChartPassRateLine = echarts.init(document.getElementById('passRateLine'))
+        this.myChartPassRateLine.setOption(this.optionPassRateLine, true)
       } catch (e) {
         this.loading = false
       }
