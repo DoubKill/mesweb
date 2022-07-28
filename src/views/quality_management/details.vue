383b5274768a9bd19cfaf66a4e46b59eb787de0d
吴恒 <813212927@qq.com>
2022-07-28 10:07:02 +0800

    tij

--- src/views/quality_management/details.vue
+++ src/views/quality_management/details.vue
@@ -149,7 +149,7 @@
         </template>
       </u-table-column>
       <u-table-column label="检测状态" prop="state" align="center" min-width="20px" />
-      <u-table-column label="处理意见" min-width="20px" prop="deal_info.deal_suggestion" align="center" />
+      <u-table-column label="处理意见" min-width="20px" prop="deal_suggestion" align="center" />
     </u-table>
     <el-alert style="color:black" title="表格背景色说明：表示不是一等品" type="success" />
     <el-dialog
