e444a168c423413b257ec1b1583705cc60f26d1c
qiny.jiang <270052939@qq.com>
2022-08-05 16:54:29 +0800

    检查确认

--- src/views/spot_check/temperature-confirm.vue
+++ src/views/spot_check/temperature-confirm.vue
@@ -376,7 +376,7 @@ export default {
     },
     confirm() {
       if (this.multipleSelection.length > 0) {
-        if (this.multipleSelection.every(d => d.status === '已检查')) {
+        if (this.multipleSelection.some(d => d.status !== '已检查')) {
           this.$message.info('确认数据只能选择已检查数据')
           return
         }
