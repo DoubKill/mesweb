1e4e018e61ffb34ae530d1b02ca3e8a4b434a33c
吴恒 <813212927@qq.com>
2022-08-17 14:15:47 +0800

    提交 密炼扫码结果查询

--- src/router/index.js
+++ src/router/index.js
@@ -519,7 +519,7 @@ export let asyncRoutes = [
             meta: {
               faName: 'Resume',
               title: '密炼扫码结果查询',
-              permissionName: 'scan_message'
+              permissionName: 'batch_scan_log'
             }
           },
           {
