e9f887734ba1c55ceb9f07ef743250d1192af76c
吴恒 <813212927@qq.com>
2022-08-18 14:49:19 +0800

    提交 输入角色+原材料配比查询

--- src/views/rubber_recipe_standard_manage/material-compare.vue
+++ src/views/rubber_recipe_standard_manage/material-compare.vue
@@ -29,7 +29,7 @@
           clearable
         />
       </el-form-item>
-      <el-form-item label="配方类别">
+      <el-form-item label="胶料类别">
         <el-select
           v-model="search.recipe_type"
           clearable
@@ -94,6 +94,10 @@
           type="index"
           width="50"
           label="No"
+        />
+        <el-table-column
+          prop="recipe_type"
+          label="胶料类别"
         />
         <el-table-column
           prop="stage_product_batch_no"
@@ -216,7 +220,21 @@ export default {
         this.tableData = data.results || []
         this.total = data.count
         this.loading = false
-        this.headList = this.search.material_names
+        const arr = []
+        this.search.material_names.forEach(d => {
+          let bool = true
+          for (let index = 0; index < this.tableData.length; index++) {
+            const element = this.tableData[index]
+            bool = element.detail.every(dd => dd.material_name !== d) // 都没有为true
+            if (!bool) {
+              break
+            }
+          }
+          if (!bool) {
+            arr.push(d)
+          }
+        })
+        this.headList = arr
 
         if (this.btnExportLoad) {
           this.tableData1 = data.results
