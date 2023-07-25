554e45e34659fe787d09ca94960d0457034a2bef
吴恒 <813212927@qq.com>
2023-07-25 09:18:55 +0800

    提交

--- src/views/production/shift-output/index.vue
+++ src/views/production/shift-output/index.vue
@@ -156,7 +156,7 @@ export default {
       const arr = []
       this.tableData.forEach(d => {
         // 计算日均产量（日均产量=total_trains/days）和完成率（日均产量/目标值）
-        d.dailyOutput = d.total_trains && d.days ? Math.round((d.total_trains / d.days) * 100) / 100 : ''
+        d.dailyOutput = d.total_trains && d.days ? Math.round((d.total_trains / d.days / 2) * 100) / 100 : ''
         d.completionRate = d.dailyOutput && d.target_trains ? Math.round((d.dailyOutput / d.target_trains / 2 * 100) * 100) / 100 + '%' : ''
         // end
         // 获取每一排的不重复日期 总计使用
